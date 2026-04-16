db5733512bbc96063e78171126dbf9a34e2d856f
Alphatom-dynamics <alphatomdynamics@gmail.com>
2026-04-16 15:29:05 +0500

    feat: add ticker components and styling to Home and AllPosts pages

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, PostCard } from "../components";
 import service from "../appwrite/service";
-
+import "./PStyling/Home.css";
 function AllPosts() {
   const [posts, setPosts] = useState([]);
 
@@ -16,12 +16,30 @@ function AllPosts() {
   return (
     <div className="w-full py-8">
       <Container>
+        <div className="ticker-wrapper">
+          <div className="ticker">
+            <span>
+              Publish your articles on NeuroByteTalks &nbsp; • &nbsp; Publish
+              your articles on NeuroByteTalks &nbsp; • &nbsp; Publish your
+              articles on NeuroByteTalks
+            </span>
+          </div>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {posts.map((post) => (
             <div key={post.$id} className="w-full">
               <PostCard {...post} />
             </div>
           ))}
+        </div>
+        <div className="ticker-wrapper2">
+          <div className="ticker">
+            <span>
+              Your content. Your voice. Your platform&nbsp; • &nbsp; Publish
+              your articles on NeuroByteTalks&nbsp; • &nbsp; Your content. Your
+              voice. Your platform
+            </span>
+          </div>
         </div>
       </Container>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import service from "../appwrite/service";
 import { Container, PostCard } from "../components";
+import "./PStyling/Home.css";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -55,12 +56,30 @@ function Home() {
   return (
     <div className="w-full py-8">
       <Container>
+        <div className="ticker-wrapper">
+          <div className="ticker">
+            <span>
+              Publish your articles on NeuroByteTalks &nbsp; • &nbsp; Publish
+              your articles on NeuroByteTalks &nbsp; • &nbsp; Publish your
+              articles on NeuroByteTalks
+            </span>
+          </div>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {posts.map((post) => (
             <div key={post.$id} className="w-full">
               <PostCard {...post} />
             </div>
           ))}
+        </div>
+        <div className="ticker-wrapper2">
+          <div className="ticker">
+            <span>
+              Your content. Your voice. Your platform&nbsp; • &nbsp; Publish
+              your articles on NeuroByteTalks&nbsp; • &nbsp; Your content. Your
+              voice. Your platform
+            </span>
+          </div>
         </div>
       </Container>
     </div>
